32e4906cd94759130e57cfccf76f59d9e464b3e9
choiseona <52223965+choiseona@users.noreply.github.com>
2023-01-13 10:40:46 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	int arr[100001];
+	int dp[100001];
+	cin >> N;
+
+	//수열 입력
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+	//규칙 찾기
+	dp[0] = arr[0];
+	//dp[1] = max(dp[0] + arr[1], arr[1]);
+	//dp[2] = max(arr[0] + arr[1] + arr[2], arr[1] + arr[2], arr[2]);
+	//dp[4] = max(arr[0] + arr[1] + arr[2] + arr[3], arr[1] + arr[2] + arr[3], arr[2] + arr[3], arr[3]);
+	//dp[i] = max(dp[i - 1] + arr[i], arr[i]);
+
+	//각 단계에서의 최대값을 dp배열에 저장
+	for (int i = 1; i < N; i++) {
+		dp[i] = max(dp[i - 1] + arr[i], arr[i]);
+	}
+
+	//내림차순 정렬해서 제일 앞의 원소(가장 큰 수) 출력
+	sort(dp, dp + N, greater<>());
+	cout << dp[0];
+}
+
