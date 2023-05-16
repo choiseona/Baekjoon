39709a2f1f1e39b5ab89f392fbc9db42a0888abf
choiseona <52223965+choiseona@users.noreply.github.com>
2023-05-16 16:06:48 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 string solution(vector<string> seoul) {
     string answer = "";
+    
     for(int i=0;i<seoul.size();i++){
         if(seoul[i]=="Kim"){
            answer = answer + "김서방은 " + to_string(i) + "에 있다";
+            return answer;
         }
     }
-    return answer;
 }
