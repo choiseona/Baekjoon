26a1c47c87f93ffa5b4f8ec145767e163f76416f
choiseona <52223965+choiseona@users.noreply.github.com>
2023-11-26 11:17:56 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int T;
+	cin >> T;
+
+	while (T--) {
+		int A, B;
+		cin >> A >> B;
+		cout << A + B << "\n";
+	}
+
+	return 0;
+}
