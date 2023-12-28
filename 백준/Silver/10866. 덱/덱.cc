bca2c1fd056285c6aee389eb7648e0cb58818c4b
choiseona <52223965+choiseona@users.noreply.github.com>
2023-12-28 20:51:53 +0900

    [Silver IV] Title: 덱, Time: 336 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main() {
+	int N, X;
+	cin >> N;
+
+	deque <int> dq;
+
+	while (N--) {
+		string oper;
+		cin >> oper;
+
+		if (oper == "push_front") {
+			cin >> X;
+			dq.push_front(X);
+		}
+		else if (oper == "push_back" ) {
+			cin >> X;
+			dq.push_back(X);
+		}
+		else if (oper == "pop_front") {
+			if (dq.empty()) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout << dq.front() << "\n";
+				dq.pop_front();
+			}
+		}
+		else if (oper == "pop_back") {
+			if (dq.empty()) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout << dq.back() << "\n";
+				dq.pop_back();
+			}
+
+		}
+		else if (oper == "size") {
+			cout << dq.size() << "\n";
+		}
+		else if (oper == "empty") {
+			cout << dq.empty() ? 1 : 0;
+			cout << "\n";
+		}
+		else if (oper == "front") {
+			if (dq.empty()) cout << -1;
+			else cout << dq.front();
+			cout << "\n";
+		}
+		else if (oper == "back") {
+			if (dq.empty()) cout << -1;
+			else cout << dq.back();
+			cout << "\n";
+		}
+	}
+	return 0;
+}
