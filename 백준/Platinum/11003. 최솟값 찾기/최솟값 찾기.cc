51edd0474eedf18e43cb6d3a3f17b87a7ababebd
choiseona <52223965+choiseona@users.noreply.github.com>
2024-01-02 14:18:22 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 1432 ms, Memory: 63120 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<deque>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N, L;
+	cin >> N >> L;
+
+	vector <int> A(N+1);
+
+	deque<pair<int,int>> dq;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> A[i];
+	}
+
+	/*
+	
+	1 5 2 3 6 2 3 7 3 5 2 6
+	2*i+1이 L의 배수이면 맨 앞 삭제
+	 
+	i=1 -> A(-1) ~ A(1) 1 => 큐에 1 삽입(1) -> 1
+	i=2 -> A(0) ~ A(2) 1,5 => 큐에 5 삽입(1,5) -> 1
+	i=3 -> A(1) ~ A(3) 1,5,2 => 큐에서 5 삭제, 2 삽입(1,2) -> 1 
+	i=4 -> A(2) ~ A(4) 5,2,3 => (맨 앞 삭제) -> 큐에 3 삽입(2,3) -> 2
+	i=5 -> A(3) ~ A(5) 2,3,6 => 큐에 6 삽입(2,3,6) -> 2 
+	i=6 -> A(4) ~ A(6) 3,6,2 => (맨 앞 삭제) -> 큐에 6,3 삭제 2 삽입 (2) -> 2
+	i=7 -> A(5) ~ A(7) 6,2,3 => 큐에 3 삽입(2,3) -> 2 
+
+	*/
+
+	for (int i = 1; i <= N; i++) {
+		if (!dq.empty() && (i - (dq.front().first)) % L == 0) dq.pop_front();
+		while (!dq.empty() && (dq.back().second > A[i])) dq.pop_back();
+		//if (!dq.empty() && ((2 * i + 1) % L) ) dq.pop_front();
+		dq.push_back({ i,A[i] });
+
+		cout << dq.front().second << " ";
+	}
+
+}
