a42299918d9f8088f930478ccaf7f2e64c3825f5
choiseona <52223965+choiseona@users.noreply.github.com>
2024-04-12 21:26:21 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,6 +1,5 @@
 function solution(money) {
     const count = Math.floor(money/5500);
     const remain = money % 5500;
-    const answer = [count,remain];
-    return answer;
+    return [count,remain];
 }
