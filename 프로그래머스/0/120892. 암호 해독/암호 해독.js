6d3700468da70666a23e2f25192e4bfcfa9296af
choiseona <52223965+choiseona@users.noreply.github.com>
2024-04-12 23:46:11 +0900

    [level 0] Title: 암호 해독, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -1,3 +1,3 @@
 function solution(cipher, code) {
-   return [...cipher].filter((word,index) => (index + 1) % code === 0 ? word : "").join("");
+   return [...cipher].filter((word,index) => (index + 1) % code === 0).join("");
 }
