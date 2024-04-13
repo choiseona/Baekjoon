08337dead20a05c7910c999d18364e22c35c2fcf
choiseona <52223965+choiseona@users.noreply.github.com>
2024-04-13 16:54:41 +0900

    [level 0] Title: 외계행성의 나이, Time: 1.98 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,4 +1,4 @@
 function solution(age) {
-    return [...String(age)].map((number) => String.fromCharCode( 'a'.charCodeAt() + number.charCodeAt() - "1".charCodeAt()+1)).join("");
- 
+    console.log("c" + 3)
+    return [...String(age)].map((number) => String.fromCharCode( 'a'.charCodeAt() + Number(number))).join("");
 }
