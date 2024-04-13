9a0aedc1f43917ccb8e8791257786a4c3f94a95f
choiseona <52223965+choiseona@users.noreply.github.com>
2024-04-13 15:56:26 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,6 +1,4 @@
 function solution(s1, s2) {
-    const hasSameWord = (words, word) => {
-        return !!words.find((element) => element === word)
-    }
-    return s1.reduce((pre,cur) => pre + hasSameWord(s2, cur),0);
+
+    return s1.filter((word) => s2.includes(word)).length;
 }
