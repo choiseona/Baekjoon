a783e4bd5d3bf0bb45cf0dfd77081010a21c305f
choiseona <52223965+choiseona@users.noreply.github.com>
2024-04-13 18:45:37 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,17 +1,4 @@
 function solution(emergency) {
     const sortedEmergency = [...emergency].sort((a,b) => b - a);
-    
-    const map = new Map();
-    
-    for(let i=0; i<sortedEmergency.length; i++) {
-        map.set(sortedEmergency[i],i+1);
-    }
- 
-    const answer = [];
-    
-    for(let i=0; i<emergency.length; i++){
-        answer.push(map.get(emergency[i]));
-    }
-    
-    return answer;
+    return emergency.map((number) => sortedEmergency.indexOf(number)+1)
 }
