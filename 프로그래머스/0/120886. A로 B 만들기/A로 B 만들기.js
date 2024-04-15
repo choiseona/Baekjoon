00b8f4c4ce5feeaeea468bf09104510e4cdad1cb
choiseona <52223965+choiseona@users.noreply.github.com>
2024-04-15 18:50:12 +0900

    [level 0] Title: A로 B 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,11 +1,3 @@
 function solution(before, after) {
-    sortedBefore = [...before].sort();
-    sortedAfter = [...after].sort();
-    
-    for(let i=0; i<before.length; i++){
-        if(sortedBefore[i] !== sortedAfter[i]){
-            return 0;
-        }
-    }
-    return 1;
+    return Number([...before].sort().join('') === [...after].sort().join(''));
 }
