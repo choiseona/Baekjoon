3eb08eb52d48f937f013e8c0776cfdab74f54a94
choiseona <52223965+choiseona@users.noreply.github.com>
2024-04-15 19:36:37 +0900

    [level 0] Title: 가까운 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -0,0 +1,15 @@
+function solution(array, n) {
+    let answer= array[0];
+
+    for(let i=1; i<array.length; i++) {
+        if(Math.abs(answer-n) > Math.abs(array[i]-n)){
+            answer = array[i] ;
+        }
+        else if(Math.abs(answer-n) === Math.abs(array[i]-n)) {
+            answer = answer > array[i] ? array[i] : answer;
+
+        }
+    }
+    
+    return answer;
+}
