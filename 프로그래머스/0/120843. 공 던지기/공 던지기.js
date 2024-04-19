4236d54a46325ac5a5b6347e36424168781ee222
choiseona <52223965+choiseona@users.noreply.github.com>
2024-04-19 16:33:02 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,11 +1,3 @@
 function solution(numbers, k) {
-    let answer = 0;
-    
-    for(let i=1; i<=k-1; i++) {
-        answer+=2;
-        if(answer > numbers.length-1)  {
-            answer %= numbers.length;
-        }
-    }
-    return numbers[answer];
+    return numbers[(--k*2) % numbers.length];
 }
