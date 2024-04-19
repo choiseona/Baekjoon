5a89ff5a74fc7d1ec93bc1eab7f942015f1eb982
choiseona <52223965+choiseona@users.noreply.github.com>
2024-04-19 15:30:50 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,10 +1,9 @@
 function solution(my_str, n) {
     let answer = [];
     
-    for(let i=0; i<=my_str.length; i+=n){
-        answer.push(my_str.slice(i,i+n))
+    for(let i=0; i<my_str.length; i+=n){
+        answer.push(my_str.substring(i,i+n))
     }
     
-    if(my_str.length % n === 0) answer.pop();
     return answer;
 }
