36015156f836e1effda852e4cbec83f2390d7ad6
choiseona <52223965+choiseona@users.noreply.github.com>
2024-04-22 15:38:27 +0900

    [level 0] Title: 등수 매기기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -1,18 +1,5 @@
 function solution(score) {
-    const sortedScore = [...score].sort((a,b) => (b[0]+b[1])/2 - (a[0]+a[1])/2)
-    const scoreMap = new Map();
-    
-    scoreMap.set(sortedScore[0],1);
-    for(let i=1; i<sortedScore.length; i++) {
-        const targetAverage = (sortedScore[i-1][0] + sortedScore[i-1][1]) / 2;
-        const average = (sortedScore[i][0] + sortedScore[i][1]) / 2;
-        
-        if(targetAverage === average) {
-            scoreMap.set(sortedScore[i],scoreMap.get(sortedScore[i-1]));
-        } else {
-            scoreMap.set( sortedScore[i], i+1);
-        }
-    }
-    
-    return score.map((item) => scoreMap.get(item));
+    return score.map((scores) => {
+        return score.filter((item) => (item[0] + item[1]) / 2 > (scores[0] + scores[1]) / 2 ).length + 1;
+    })
 }
