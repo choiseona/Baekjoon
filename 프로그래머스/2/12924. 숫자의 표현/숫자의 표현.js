4167950e253cfff94028737fec568cf631432364
choiseona <52223965+choiseona@users.noreply.github.com>
2024-04-30 19:02:43 +0900

    [level 2] Title: 숫자의 표현, Time: 0.25 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,9 +1,7 @@
 function solution(n) {
     let oddCount = 0;
     for(let i=1; i<=n; i++) {
-        
-        if(i % 2 != 0 && n % i === 0)oddCount++;
-        
+        if(i % 2 !== 0 && n % i === 0)oddCount++;
     }
     return oddCount;
 }
