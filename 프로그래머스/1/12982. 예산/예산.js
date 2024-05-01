09c6103929a301ba7007ac8098e45778d7db837f
choiseona <52223965+choiseona@users.noreply.github.com>
2024-05-01 16:39:00 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,12 @@
+function solution(d, budget) {
+    let sum = 0;
+    let answer = 0;
+  
+    d.sort((a,b) => a-b);
+    
+    for(let i=0; i<d.length; i++) {
+        sum += d[i];
+        if(sum <= budget) answer++;
+    }
+    return answer;
+}
