f03751c253a7d9459fbbb588bbb518f7852ab0a9
choiseona <52223965+choiseona@users.noreply.github.com>
2024-05-01 21:11:38 +0900

    [level 1] Title: 폰켓몬, Time: 0.22 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,7 +1,5 @@
 function solution(nums) {
-    const numberSet = new Set();
-    nums.forEach((e) => numberSet.add(e));
-    const setLength = numberSet.size;
+    const setLength = new Set(nums).size;
     const maxLength = nums.length / 2;
     
     return maxLength >= setLength ? setLength : maxLength; 
