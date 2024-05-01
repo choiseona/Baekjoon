721fec8c659eaf71cb5ec21608a6ff0369098c25
choiseona <52223965+choiseona@users.noreply.github.com>
2024-05-01 16:46:13 +0900

    [level 1] Title: 예산, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,12 +1,5 @@
 function solution(d, budget) {
-    let sum = 0;
-    let answer = 0;
-  
-    d.sort((a,b) => a-b);
-    
-    for(let i=0; i<d.length; i++) {
-        sum += d[i];
-        if(sum <= budget) answer++;
-    }
-    return answer;
+    return d.sort((a,b) => a-b).reduce((count,price) => {
+        return count + ((budget -= price) >= 0);
+    },0)
 }
