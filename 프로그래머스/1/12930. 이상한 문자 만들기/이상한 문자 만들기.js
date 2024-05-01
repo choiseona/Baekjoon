ffc40e6b43122ca11ef036b3911d8812c8b01bf0
choiseona <52223965+choiseona@users.noreply.github.com>
2024-05-01 16:09:18 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,13 +1,3 @@
 function solution(s) {
-    let answer = '';
-    const targetS = s.split(" ");
-    
-    for(let i=0; i<targetS.length; i++) {
-        for(let j=0; j<targetS[i].length; j++) {
-            answer += j % 2 === 0 ? targetS[i][j].toUpperCase() : targetS[i][j].toLowerCase();
-        }
-        if(i !== targetS.length-1)  answer+=" ";
-    }
-    
-    return answer;
+    return s.split(" ").map(word => word.split("").map((spell,sIndex) => sIndex % 2 === 0 ? spell.toUpperCase(): spell.toLowerCase()).join('')).join(" ");
 }
