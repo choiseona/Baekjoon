ec4c560de327ae5708981ddb09f9815201cada6f
choiseona <52223965+choiseona@users.noreply.github.com>
2024-05-03 14:43:26 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 45.60 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,11 +1,16 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
+    const participantMap = new Map();
     
-    for(let i=0; i<participant.length; i++){
-        if(participant[i] !== completion[i]){
-            return participant[i];
-        }
+    participant.forEach((e, i) => {
+        participantMap.set(e,(participantMap.get(e)||0)+1);
+    })
+    
+    completion.forEach((e,i) => {
+        participantMap.set(e, (participantMap.get(e) || 0)-1);
+    })
+    
+    for(let [key, value] of participantMap) {
+        if(value > 0) return key;
     }
 
 }
