50c5f6504c1e10ecee5efe1d93202537f08a9e1f
choiseona <52223965+choiseona@users.noreply.github.com>
2024-05-06 16:25:22 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -2,8 +2,6 @@ function solution(operations) {
     const queue = [];
     
     while(operations.length !== 0) {
-        queue.sort((a,b) => a - b); // 오름차순
-        
         const [operation, number] = operations.shift().split(" ");
         if(operation === "I") {
             queue.push(parseInt(number));
@@ -14,10 +12,10 @@ function solution(operations) {
         else if(operation === "D" && number === "-1" && queue.length > 0) {
             queue.shift();
         }
+        queue.sort((a,b) => a - b); // 오름차순
     }
     
-    const min = queue.length === 0 ? 0 : Math.min(...queue);
-    const max = queue.length === 0 ? 0 : Math.max(...queue);
-    
-    return [max,min];
+    if(queue.length === 0) return [0,0];
+    else return [queue[queue.length-1], queue[0]];
+
 }
