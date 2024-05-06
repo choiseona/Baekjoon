55b51f38d461738bbdd5d2dac66244e43a6aef44
choiseona <52223965+choiseona@users.noreply.github.com>
2024-05-06 16:49:34 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,9 +1,6 @@
 function solution(array, commands) {
-    const answer = [];
-    for(let iter=0; iter<commands.length; iter++) {
-        const [i, j ,k] = commands[iter];
-        const number = array.slice(i-1 ,j).sort((a,b) => a-b)[k-1];
-        answer.push(number)
-    }
-    return answer;
+    return commands.map((e) => {
+        const [i, j ,k] = e;
+        return array.slice(i-1 ,j).sort((a,b) => a-b)[k-1];
+    }) 
 }
