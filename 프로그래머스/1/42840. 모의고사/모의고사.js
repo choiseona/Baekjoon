f50c380ec3c45ed67490d04f7119cf70924c19ec
choiseona <52223965+choiseona@users.noreply.github.com>
2024-05-07 13:16:08 +0900

    [level 1] Title: 모의고사, Time: 1.74 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,25 +1,20 @@
 function solution(answers) {
     let answer=[];
+    let scores=[];
     const failerChoices = [[1,2,3,4,5],[2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]];
     
-    failerChoices.forEach((failerChoice, studentIndex) => {
-        let score = 0;
-        answers.forEach((answer,i) => {
-            const index = i % failerChoice.length;
-            if(failerChoice[index] === answer) score++;
-        })
-        
-        // 이전 수포자 점수보다 낮으면 push x,
-        // 이전 수포자 점수와 같으면 push,
-        // 이전 수포자 점수보다 크면 배열 비우고 push
-        // => 결론적으로 가장 큰 점수를 가진 학생만 저장
-        const maxScore = answer[0]?.[0];
-    
-        if(answer.length === 0) answer.push([score, studentIndex + 1]);
-        else if(maxScore === score) answer.push([score, studentIndex + 1]);
-        else if(maxScore < score) answer = [[score, studentIndex + 1]];
-        
+    // 각 점수 계산
+    failerChoices.forEach((failerChoice) => {
+        const score = answers.filter((answer,index) => {
+             return answer === failerChoice[(index % failerChoice.length)]
+        }).length;
+        scores.push(score);
     })
     
-    return answer.reduce((cul,cur) => [...cul,cur[1]], [])
+    // 최대값 추출하여 답(인덱스) 구하기
+    const max = Math.max(...scores)
+    scores.forEach((score,index) => score === max && answer.push(index+1));
+    
+    return answer;
+    
 }
