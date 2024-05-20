8ae3b56f249c950ea595320778d9dd49460e5220
choiseona <52223965+choiseona@users.noreply.github.com>
2024-05-20 16:16:36 +0900

    [level 1] Title: 체육복, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,20 +1,19 @@
 function solution(n, lost, reserve) {   
     // 체육복을 스스로 가져온 학생
     let originalStudent = n - lost.length;
-
-    // 체육복을 빌릴 수 있는 학생
-    const alreadyReserving = [];
-    let sortedReserving = reserve.sort((a,b) => a-b);
-    let sortedLost = lost.sort((a,b) => a-b);
     
     // 체육복 여벌을 가져왔는데 도난당한 학생을 reserve에서 제외시키고 스스로 가져온 학생에 카운트
-    const commonElements = sortedLost.filter(value => sortedReserving.includes(value));
+    const commonElements = lost.filter(value => reserve.includes(value));
     originalStudent+= commonElements.length;
 
-    sortedLost = sortedLost.filter(value => !commonElements.includes(value));
-    sortedReserving = sortedReserving.filter(value => !commonElements.includes(value));    
+    lost = lost.filter(value => !commonElements.includes(value));
+    reserve = reserve.filter(value => !commonElements.includes(value));    
         
-    // 
+    // 체육복을 빌릴 수 있는 학생
+    const sortedReserving = reserve.sort((a,b) => a-b);
+    const sortedLost = lost.sort((a,b) => a-b);
+    const alreadyReserving = [];
+    
     for(let i=0; i<sortedLost.length; i++) {
         for(let j=0; j<sortedReserving.length; j++) {
             const difference = Math.abs(sortedLost[i] - sortedReserving[j]);
