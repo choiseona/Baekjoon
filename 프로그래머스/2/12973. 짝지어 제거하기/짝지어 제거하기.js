80fa4e01eab88690ec7d69aa5960ccfd1a7cf097
choiseona <52223965+choiseona@users.noreply.github.com>
2024-05-23 19:08:05 +0900

    [level 2] Title: 짝지어 제거하기, Time: 25.30 ms, Memory: 46.5 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -8,16 +8,11 @@ function solution(s) {
         if(stack.length === 0) stack.push(target);
         
         // stack 안비었고 같은 문자열이면 stack의 top이 같은 문자열이 아닐때까지 stack.pop
-        else if(stack[stack.length -1] === target){
-            while(stack[stack.length-1] === target) {
-                stack.pop();
-            }
-        }
-        
+        else if(stack[stack.length -1] === target) stack.pop();
+           
         // stack 안비었고 같은 문자열이 아니면 push
-        else if(stack[stack.length -1] !== target) {
-            stack.push(target);
-        }
+        else if(stack[stack.length -1] !== target) stack.push(target);
+        
     }
     
     return stack.length === 0 ? 1 : 0;
