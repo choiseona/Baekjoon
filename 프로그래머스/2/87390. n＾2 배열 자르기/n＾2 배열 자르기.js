452a8bb03660a66cae5207f878f5a03a049fc7fa
choiseona <52223965+choiseona@users.noreply.github.com>
2024-05-29 19:31:26 +0900

    [level 2] Title: n^2 배열 자르기, Time: 5.19 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,10 +1,8 @@
 function solution(n, left, right) {
     const answer = [];
     
-    for(let i=left+1; i<=right+1; i++) {
-        const x = Math.ceil(i/n);
-        const y = i - n*(x-1);
-        answer.push(Math.max(x,y))
+    for(let i=left; i<=right; i++) {
+        answer.push(Math.max(Math.floor(i/n), i%n) + 1);
     }
     
     return answer;
