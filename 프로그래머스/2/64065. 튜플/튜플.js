82636090f635c7b23f5905d0d586faada504a743
choiseona <52223965+choiseona@users.noreply.github.com>
2024-05-31 22:29:59 +0900

    [level 2] Title: 튜플, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,8 +1,5 @@
 function solution(s) {
-    s = s.replace(/{/g,'[')
-    s = s.replace(/}/g,']');
-    s = JSON.parse(s);
-    s = s.sort((a,b) => a.length - b.length);
+    s = JSON.parse(s.replace(/{/g,'[').replace(/}/g,']')).sort((a,b) => a.length - b.length);
     
     const visitedNumber = [];
     const set = [];
