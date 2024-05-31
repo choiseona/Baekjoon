6720fb54a6cc1b179b5bb170f8866b32a487e770
choiseona <52223965+choiseona@users.noreply.github.com>
2024-05-31 21:27:08 +0900

    [Docs] 풀이 수정

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
@@ -7,6 +7,8 @@ function solution(land) {
     
     const map = new Map();
 
+    // 너비 우선 탐색
+    // 시간 줄이기 위해 각 석유 덩어리에 해당하는 column 반환하기
     const BFS = ([i, j], visited) => {
         const queue = [];
         let count = 0;
@@ -14,7 +16,7 @@ function solution(land) {
         
         queue.push([i, j]);
         columnSet.add(j);
-        visited[i][j] = true; // 방문 여부 체크
+        visited[i][j] = true; 
         count++;
         
         while (queue.length !== 0) {
@@ -35,6 +37,8 @@ function solution(land) {
         return [count, columnSet];
     }
     
+    // 시간 줄이기 위해 visit이 초기화되는 일 없도록하기
+    // visit 하나만 두되, 각 석유 덩어리에 해당하는 column에 대해 map으로 석유 count(key: column, value:count)
     let maxCount = 0;
     const visited = Array.from({ length: height }, () => Array(width).fill(false));
     
@@ -50,6 +54,5 @@ function solution(land) {
     }
     
     maxCount = Math.max(...map.values())
-    
     return maxCount;
 }
