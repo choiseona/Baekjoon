c35bc1388a8160e4824a698488d8a7c1294b40b1
choiseona <52223965+choiseona@users.noreply.github.com>
2024-06-02 22:43:15 +0900

    [level 1] Title: 공원 산책, Time: 0.45 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -17,25 +17,33 @@ function solution(park, routes) {
     // 공원 벗어나거나 장애물 있으면 해당 명령 무시하고 다음 명령 수행
     const roadYLength = road.length;
     const roadXLength = road[0].length;
-    const isExistHurdle = ([departY, departX], [arriveY, arriveX]) => {
-        let isHurdle = false;
-        if(arriveY - departY < 0) [arriveY, departY] = [departY, arriveY];
-        if(arriveX - departX < 0) [arriveX, departX] = [departX, arriveX];
-        for(let i=departY; i<=arriveY; i++) {
-            for(let j=departX; j<=arriveX; j++) {
-                if(road[i][j] === "X") isHurdle = true;
+    const dir = {"E":[0,1], "W":[0,-1], "N":[-1,0], "S":[1,0]};
+    
+const isExistHurdle = ([arriveY, arriveX]) => {
+    let isHurdle = false;
+    const startX = Math.min(dogX, arriveX);
+    const startY = Math.min(dogY, arriveY);
+    const endX = Math.max(dogX, arriveX);
+    const endY = Math.max(dogY, arriveY);
+    for (let i = startY; i <= endY; i++) {
+        for (let j = startX; j <= endX; j++) {
+            if (road[i][j] === "X") {
+                isHurdle = true;
+                break;
             }
         }
-        return isHurdle;
+        if (isHurdle) break;
     }
+    return isHurdle;
+}
     
     routes.forEach((route) => {
-        let [direction, count] = route.split(" ");
-        count = parseInt(count);
-        if(direction === "E" && dogX + count < roadXLength && !isExistHurdle([dogY, dogX], [dogY, dogX+count])) dogX+=count;
-        else if(direction === "W" && dogX - count >=0 && !isExistHurdle([dogY, dogX], [dogY, dogX-count])) dogX-=count
-        else if(direction === "N" && dogY - count >= 0 && !isExistHurdle([dogY, dogX], [dogY - count, dogX])) dogY-=count
-        else if(direction === "S" && dogY + count < roadYLength && !isExistHurdle([dogY, dogX], [dogY + count, dogX])) dogY+=count;
+        let [direction, count] = route.split(" "); count = parseInt(count);
+        const nextDogXLoction = dogX + count * dir[direction][1],  nextDogYLocation = dogY + count * dir[direction][0];
+        if(nextDogXLoction < 0 || nextDogXLoction >= roadXLength || nextDogYLocation < 0 || nextDogYLocation >= roadYLength) return;
+        if(isExistHurdle([dogY, nextDogXLoction]) || isExistHurdle([nextDogYLocation, dogX] )) return;
+
+        dogX = nextDogXLoction; dogY = nextDogYLocation;
         
     }) 
 
