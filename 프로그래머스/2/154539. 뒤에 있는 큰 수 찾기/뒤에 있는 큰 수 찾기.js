2c6cea569d45a4487fd091d7e751d0f7ecbe6691
choiseona <52223965+choiseona@users.noreply.github.com>
2024-06-08 02:30:28 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 103.84 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,16 +1,15 @@
 function solution(numbers) {
     const stack = [], answer = Array.from({length:numbers.length}, () => -1);
     
-    stack.push({index:0, number:numbers[0]});
+    stack.push(0);
     
     for(let i=1; i<numbers.length; i++) {
         // 앞에 작은 숫자가 있을 때 까지만 stack.pop 하고 answer 갱신 
-        while(stack.length > 0 && stack[stack.length-1].number < numbers[i]) {
-            const {index} = stack.pop();
-            answer[index] = numbers[i];
+        while(stack.length > 0 && numbers[stack[stack.length-1]] < numbers[i]) {
+            answer[stack.pop()] = numbers[i];
         }
         // 현재 숫자 정보 stack에 push
-        stack.push({index:i, number:numbers[i]});
+        stack.push(i);
     }      
     
     return answer;
