27885ef787167a3ba53429b107ffa9fb1bc55415
choiseona <52223965+choiseona@users.noreply.github.com>
2024-06-10 14:04:16 +0900

    [level 2] Title: 숫자 변환하기, Time: 64.16 ms, Memory: 42.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,39 @@
+function solution(x, y, n) {
+    let minCount = Infinity;
+    
+    const BFS = (startNumber) => {
+        const queue = [];
+        const visited = Array.from({length:y+1}, () => false);
+        
+        queue.push({number:startNumber, count:0});
+        visited[startNumber] = true;
+        
+        while(queue.length) {
+            const {number, count} = queue.shift();
+            if(number === x) minCount = Math.min(count, minCount)
+            
+            const numberMinusN = number - n;
+            if(numberMinusN >= x && !visited[numberMinusN]) {
+                queue.push({number:numberMinusN, count:count+1});
+                visited[numberMinusN] = true;
+            } 
+            
+            const numberDivided2 = number / 2;
+            if(number % 2 === 0 && numberDivided2 >= x && !visited[numberDivided2]) {
+                queue.push({number: numberDivided2, count:count+1});
+                visited[numberDivided2] = true;
+            }
+            
+            const numberDivided3 = number / 3;
+            if(number % 3 === 0 && numberDivided3 >=x && !visited[numberDivided3]) {
+                queue.push({number:numberDivided3, count:count+1});
+                visited[numberDivided3] = true;
+            } 
+            
+        }
+        return minCount;
+    }
+    
+    const answer = BFS(y);
+    return answer === Infinity ? -1 : answer;
+}
