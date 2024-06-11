74b6a45b2962769283710da650269d9ca401f2d7
choiseona <tjsdk617@naver.com>
2024-06-11 19:37:55 +0900

    [프로그래머스 level 1] 개인정보 수집 유효기간 - 150370

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,20 +1,21 @@
 function solution(today, terms, privacies) {
-    const [todayYear, todayMonth, todayDay] = today.split(".").map(Number);
-    const todayNumber = todayYear * 12 * 28 + todayMonth * 28 + todayDay;
-    
-    const termsMap = {};
-    terms.forEach((item) => {
-        const [type, validate] = item.split(" ");
-        termsMap[type] = Number(validate);
-    });
-    
-    let answer = [];
-    privacies.forEach((item,index) => {
-        const [date, type] = item.split(" ");
-        const [startYear, startMonth, startDay] = date.split(".").map(Number);
-        const endNumber = startYear * 12 * 28 + (startMonth + termsMap[type]) * 28 + startDay;
-        if(todayNumber >= endNumber) answer.push(index+1)
-    })
+  const [todayYear, todayMonth, todayDay] = today.split(".").map(Number);
+  const todayNumber = todayYear * 12 * 28 + todayMonth * 28 + todayDay;
 
-    return answer;
+  const termsMap = {};
+  terms.forEach((item) => {
+    const [type, validate] = item.split(" ");
+    termsMap[type] = Number(validate);
+  });
+
+  let answer = [];
+  privacies.forEach((item, index) => {
+    const [date, type] = item.split(" ");
+    const [startYear, startMonth, startDay] = date.split(".").map(Number);
+    const endNumber =
+      startYear * 12 * 28 + (startMonth + termsMap[type]) * 28 + startDay;
+    if (todayNumber >= endNumber) answer.push(index + 1);
+  });
+
+  return answer;
 }
