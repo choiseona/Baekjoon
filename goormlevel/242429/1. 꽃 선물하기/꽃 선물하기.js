d8a3228d68c3dfa0ef1d89bd6197962916cde2d9
choiseona <52223965+choiseona@users.noreply.github.com>
2024-06-15 21:16:37 +0900

    [goormlevel] Title: 꽃 선물하기, Time: 0.04 ms, Memory: 31.38 MB, Difficulty: 1 -BaekjoonHub

---
+++ goormlevel/242429/1. 꽃 선물하기/꽃 선물하기.js
@@ -0,0 +1,26 @@
+// Run by Node.js
+const readline = require('readline');
+
+(async () => {
+	let rl = readline.createInterface({ input: process.stdin });
+	
+	const input = [];
+	for await (const line of rl) {
+		input.push(line);
+		if(!line) rl.close();
+	}
+	const N = input[0];
+	const present = input.slice(1);
+	solution(N, present)
+	
+	process.exit();
+})();
+
+function solution(N, present) {
+	present.forEach((item) => {
+		const [a,b] = item.split(" ").map(Number);
+		if(a < b) console.log("Sunflower");
+		else if(a ===b) console.log("Tulip");
+		else console.log("Rose")
+	})
+}
