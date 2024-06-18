93fc31214753fb1e3bf6cf31cf9a715f32c7c41e
choiseona <52223965+choiseona@users.noreply.github.com>
2024-06-18 13:09:45 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.31 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -1,8 +1,6 @@
 function solution(arrayA, arrayB) {
-    const arrayAGCD = arrayA.reduce((cul,cur) => getGCD(cul,cur));
-    const arrayBGCD = arrayB.reduce((cul,cur) => getGCD(cul,cur));
-    const arrayADivisor = getDivisor(arrayAGCD);
-    const arrayBDivisor = getDivisor(arrayBGCD);
+    const arrayADivisor = getDivisor(arrayA);
+    const arrayBDivisor = getDivisor(arrayB);
     const maxArrayADivisor = getMaxDivisor(arrayADivisor, arrayB);
     const maxArrayBDivisor = getMaxDivisor(arrayBDivisor, arrayA)
     return Math.max(maxArrayADivisor, maxArrayBDivisor)
@@ -19,21 +17,14 @@ function getMaxDivisor(arrayDivisor, array) {
     return answer || 0;
 }
 
-
-function getDivisor(number) {
+function getDivisor(array) {
+    array = array.sort((a,b) => a-b);
     const answer = [];
-    for(let i=1; i<=Math.sqrt(number); i++) {
-        if(number % i !==0) continue;
-        answer.push(i);
-        if(i !== number / i) {
-            answer.push(number / i);
+    for(let i=1; i<=array[0]; i++) {
+        if(array[0] % i !== 0) continue;
+        if(array.every(num => num % i ===0)) {
+            answer.push(i);
         }
     }
     return answer.sort((a,b) => b-a);
-}
-
-function getGCD(number1, number2) {
-    if(number2 === 0) return number1;
-    if(number1 < number2) [number2, number1] = [number1,number2]
-    return getGCD(number2, number1 % number2);
 }
