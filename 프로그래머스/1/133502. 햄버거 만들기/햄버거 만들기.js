36f55008329dc0fe7f43177fc36481e78fc829fb
choiseona <52223965+choiseona@users.noreply.github.com>
2024-06-19 18:38:41 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,15 +1,25 @@
-
 function solution(ingredient) {
+    // 1: 빵, 2:야채, 3:고기
     const stack = [];
+    const next = {1:2, 2:3 , 3:1};
     let count = 0;
-    for (const item of ingredient) {
+    let current = '';
+    
+    for(const item of ingredient) {
+        if(item === next[current[current.length-1]]) current += item;
+        else if(item === 1) current = '1';
+        else current = '';
         stack.push(item);
-        if (stack[stack.length-1] !== 1) continue;
-        if (stack[stack.length-2] !== 3) continue; 
-        if (stack[stack.length-3] !== 2) continue;
-        if (stack[stack.length-4] !== 1) continue;
-        count++;
-        stack.splice(-4);
+        
+        if(current === '1231') {
+            count++;
+            for(let i=0; i<4; i++) stack.pop();
+            if(stack.slice(stack.length-3).join("") === '123') current = '123';
+            else if(stack.slice(stack.length-2).join("") === '12') current = '12';
+            else if(stack.slice(stack.length-1).join("") === "1") current = '1';
+            else current = '';
+            
+        }
     }
     
     return count;
