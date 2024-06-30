aad2d37618d5436c5c4c56b77869e17e0be0521f
choiseona <tjsdk617@naver.com>
2024-06-30 14:44:20 +0900

    [프로그래머스 level 2] 두 큐 합 같게 만들기 - 118667

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,32 +1,32 @@
+// 투포인터 사용
 function solution(queue1, queue2) {
-    const totalQueue = [...queue1, ...queue2];
-    const totalLength = totalQueue.length;
-    const totalSum = getSum(totalQueue);
-    const targetSum = totalSum / 2;
-    
-    if(totalSum % 2 !== 0) return -1;
-    
-    let queue1Sum = getSum(queue1);
-    let queue1Head = 0, queue1Tail = queue1.length-1;
-    let result = 0;
-    
-    while(result < 2 * totalLength) {
-        if(queue1Sum === targetSum) return result;
-        if(queue1Sum > targetSum) {
-            queue1Sum -= totalQueue[queue1Head];
-            queue1Head = (queue1Head + 1) % totalLength;
-        }
-        else if(queue1Sum < targetSum) {
-            queue1Tail = (queue1Tail + 1) % totalLength;
-            queue1Sum += totalQueue[queue1Tail]
-        }
-        result++;
+  const totalQueue = [...queue1, ...queue2];
+  const totalLength = totalQueue.length;
+  const totalSum = getSum(totalQueue);
+  const targetSum = totalSum / 2;
+
+  if (totalSum % 2 !== 0) return -1;
+
+  let queue1Sum = getSum(queue1);
+  let queue1Head = 0,
+    queue1Tail = queue1.length - 1;
+  let result = 0;
+
+  while (result < 2 * totalLength) {
+    if (queue1Sum === targetSum) return result;
+    if (queue1Sum > targetSum) {
+      queue1Sum -= totalQueue[queue1Head];
+      queue1Head = (queue1Head + 1) % totalLength;
+    } else if (queue1Sum < targetSum) {
+      queue1Tail = (queue1Tail + 1) % totalLength;
+      queue1Sum += totalQueue[queue1Tail];
     }
-    
-    return -1;
-    
+    result++;
+  }
+
+  return -1;
 }
-    
+
 function getSum(arr) {
-    return arr.reduce((acc,cur) => acc+ cur,0);
+  return arr.reduce((acc, cur) => acc + cur, 0);
 }
